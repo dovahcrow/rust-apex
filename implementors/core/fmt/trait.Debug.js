13c8e478325301ee26dba6421b8402e84e89647d
Wu Young <doomsplayer@gmail.com>
2017-02-27 03:05:43 +0000

    (cargo-release) generate docs

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rust_apex"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='trait core::fmt::Debug'>Debug</a> for <a class='struct' href='rust_apex/struct.Context.html' title='struct rust_apex::Context'>Context</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/rust_apex/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rust_apex"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
